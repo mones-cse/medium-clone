d382c0b05659c6acd85820e7833831f9efffc442
mones <mones.cse@gmail.com>
2022-06-20 20:00:51 +0600

    fetch data for dynamic route

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,66 @@
+import { sanityClient, urlFor } from "../../sanity";
+import Header from "../../components/Header";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { Post } from "../../typing";
+import { useRouter } from "next/router";
+
+interface Props {
+  post: Post;
+}
+
+const Post = ({ post }: Props) => {
+  const router = useRouter();
+  const { slug } = router.query;
+  console.log({ post });
+  return (
+    <main>
+      <Header />
+      <div>{slug}</div>
+    </main>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async (context) => {
+  const queryForAllPost = `*[_type == "post"]{
+        _id,
+        slug{current}
+    }`;
+  const posts = await sanityClient.fetch(queryForAllPost);
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+      id: post._id,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking", // false or 'blocking'
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const slug = context.params && context.params.slug;
+  const queryForPost = `*[_type == "post" && slug.current=="${slug}"][0]{
+          _id,
+          title,
+          author -> {
+          name,
+          image
+          },
+        description,
+        mainImage,
+        slug
+        }`;
+  const post = await sanityClient.fetch(queryForPost);
+  if (post.length == 0) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: { post }, // will be passed to the page component as props
+  };
+};
+
+export default Post;
--- typing.d.ts
+++ typing.d.ts
@@ -1,5 +1,5 @@
 export interface Post {
-  id: string;
+  _id: string;
   createdAt: string;
   updatedAt: string;
   author: {
