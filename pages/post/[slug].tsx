61e327b8131d29e9997b6a51e26a33ad0c274437
mones <mones.cse@gmail.com>
2022-06-21 02:02:25 +0600

    form data recive

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -5,6 +5,7 @@ import { Post } from "../../typing";
 import { useRouter } from "next/router";
 import { PortableText } from "@portabletext/react";
 import Head from "next/head";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 interface Props {
   post: Post;
@@ -87,9 +88,25 @@ const ptComponents = {
   },
 };
 
+interface iFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
+
 const Post = ({ post }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<iFormInput>();
   const router = useRouter();
   const { slug } = router.query;
+  const submitFormData: SubmitHandler<iFormInput> = (data) => {
+    console.log("submit from data called", { data });
+  };
   return (
     <div>
       <Header />
@@ -127,7 +144,7 @@ const Post = ({ post }: Props) => {
         <hr className={"my-5 mx-auto max-w-lg border border-yellow-500"} />
         {/*  ----------------------------------------------------------------------------------------------  */}
         <form
-          // onSubmit={handleSubmit(submitFormData)}
+          onSubmit={handleSubmit(submitFormData)}
           className="mx-auto mb-10 flex max-w-2xl flex-col p-5"
         >
           <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
@@ -135,7 +152,7 @@ const Post = ({ post }: Props) => {
           <hr className="mt-2 py-3" />
 
           <input
-            /*{...register("_id")}*/
+            {...register("_id")}
             type="hidden"
             name="_id"
             value={post._id}
@@ -144,7 +161,7 @@ const Post = ({ post }: Props) => {
           <label className="mb-5 block">
             <span className="text-gray-700">Name</span>
             <input
-              /*{...register("name", { required: true })}*/
+              {...register("name", { required: true })}
               className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
               type="text"
               placeholder="Enter your name"
@@ -153,7 +170,7 @@ const Post = ({ post }: Props) => {
           <label className="mb-5 block">
             <span className="text-gray-700">Email</span>
             <input
-              /*{...register("email", { required: true })}*/
+              {...register("email", { required: true })}
               className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
               type="email"
               placeholder="Enter your Email"
@@ -162,7 +179,7 @@ const Post = ({ post }: Props) => {
           <label className="mb-5 block">
             <span className="text-gray-700">Comment</span>
             <textarea
-              /*{...register("comment", { required: true })}*/
+              {...register("comment", { required: true })}
               className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
               placeholder="Comment...."
               rows={8}
@@ -171,15 +188,15 @@ const Post = ({ post }: Props) => {
 
           {/* errors will return when field validation fails */}
           <div className="flex flex-col p-5">
-            {/*{errors.name && (*/}
-            {/*  <span className="text-red-500">Name is required</span>*/}
-            {/*)}*/}
-            {/*{errors.email && (*/}
-            {/*  <span className="text-red-500">Email is required</span>*/}
-            {/*)}*/}
-            {/*{errors.comment && (*/}
-            {/*  <span className="text-red-500">Comment is required</span>*/}
-            {/*)}*/}
+            {errors.name && (
+              <span className="text-red-500">Name is required</span>
+            )}
+            {errors.email && (
+              <span className="text-red-500">Email is required</span>
+            )}
+            {errors.comment && (
+              <span className="text-red-500">Comment is required</span>
+            )}
           </div>
           <input
             type="submit"
